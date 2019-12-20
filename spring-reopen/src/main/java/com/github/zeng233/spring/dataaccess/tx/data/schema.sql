f3e8bec6878937604b07f11ca4d4e37eb15e3c09
zenghua <hua.zeng@dmall.com>
2019-12-20 18:19:32 +0800

    修改数据库驱动配置

--- spring-reopen/src/main/java/com/github/zeng233/spring/dataaccess/tx/data/schema.sql
+++ spring-reopen/src/main/java/com/github/zeng233/spring/dataaccess/tx/data/schema.sql
@@ -1,5 +1,7 @@
-drop table if exists user;
-create table user (
+create database spring;
+
+drop table if exists t_user;
+create table t_user (
   id int not null auto_increment primary key,
   user_name varchar(20),
   create_time datetime
