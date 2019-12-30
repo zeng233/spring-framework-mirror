0f4ba1c9fd549bdadde8c4fff9597611c7d18e33
zenghua <hua.zeng@dmall.com>
2019-12-30 10:17:01 +0800

    BeanUtils测试

---
+++ spring-reopen/src/main/java/com/github/zeng233/spring/container/bean/core/beans/CopyBean1.java
@@ -0,0 +1,30 @@
+package com.github.zeng233.spring.container.bean.core.beans;
+
+/**
+ * @ClassName CopyBean1
+ * @Description
+ * @Author zenghua
+ * @Email hua.zeng@dmall.com
+ * @Date 2019/12/25 17:00
+ * @Version 1.0
+ */
+public class CopyBean1 {
+    public int id;
+    public String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
