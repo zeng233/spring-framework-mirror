b7c55a0e2418d16bd8aae8ae4a19fc1cfcec83f9
zenghua <hua.zeng@dmall.com>
2020-01-21 14:52:23 +0800

    spring单元测试

---
+++ spring-reopen/src/test/java/com/github/zeng233/spring/test/ParamsTest.java
@@ -0,0 +1,12 @@
+package com.github.zeng233.spring.test;
+
+/**
+ * @Description
+ * 参考 {@link org.springframework.test.context.junit4.ParameterizedDependencyInjectionTests}
+ * @Author zenghua
+ * @Email hua.zeng@dmall.com
+ * @Date 2020/1/21 14:43
+ * @Version 1.0
+ */
+public class ParamsTest {
+}
