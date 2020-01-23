d5608e8d23ebdb19cd604ee77c7936312d2ae06e
zenghua <hua.zeng@dmall.com>
2020-01-23 13:37:54 +0800

    dispatchservlet初始化

--- spring-reopen/src/test/java/com/github/zeng233/spring/test/ParamsTest.java
+++ spring-reopen/src/test/java/com/github/zeng233/spring/test/ParamsTest.java
@@ -2,6 +2,9 @@ package com.github.zeng233.spring.test;
 
 /**
  * @Description
+ * * @Description 参考：How to run JUnit SpringJUnit4ClassRunner with Parametrized? https://stackoverflow.com/questions/28560734/how-to-run-junit-springjunit4classrunner-with-parametrized
+ *  * Using SpringJUnit4ClassRunner with Parameterized：https://www.baeldung.com/springjunit4classrunner-parameterized
+ *
  * 参考 {@link org.springframework.test.context.junit4.ParameterizedDependencyInjectionTests}
  * @Author zenghua
  * @Email hua.zeng@dmall.com
