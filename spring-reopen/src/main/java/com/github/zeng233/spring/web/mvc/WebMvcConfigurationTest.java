4c6bfbdcf94c1d577de7887d2993656a3d30c64e
zenghua <zenghua233@163.com>
2021-03-05 10:31:47 +0800

    注解初始化核心类分析

---
+++ spring-reopen/src/main/java/com/github/zeng233/spring/web/mvc/WebMvcConfigurationTest.java
@@ -0,0 +1,21 @@
+package com.github.zeng233.spring.web.mvc;
+
+import org.junit.Test;
+
+/**
+ * @Description 测试mvc注解形式加载bean
+ * @Author zenghua
+ * @Email hua.zeng@dmall.com
+ * @Date 2021/3/5 9:04
+ * @Version 1.0
+ */
+public class WebMvcConfigurationTest {
+
+    /**
+     * DelegatingWebMvcConfiguration类为mvcconfig注解入口
+     */
+    @Test
+    public void simple() {
+
+    }
+}
