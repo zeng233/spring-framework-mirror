bccd6fc9efce8b32e536eea5affdd2b4961d2d2e
曾华(10039614) <zengh@glodon.com>
2022-01-15 15:02:39 +0800

    exception test

---
+++ spring-reopen/src/main/java/com/github/zeng233/spring/dataaccess/tx/exception/TxExceptionTest.java
@@ -0,0 +1,11 @@
+package com.github.zeng233.spring.dataaccess.tx.exception;
+
+/**
+ * 异常测试：
+ * 用例1：Transaction rolled back because it has been marked as rollback-only
+ *
+ * @author zenghua
+ * @date 2022/1/15
+ */
+public class TxExceptionTest {
+}
