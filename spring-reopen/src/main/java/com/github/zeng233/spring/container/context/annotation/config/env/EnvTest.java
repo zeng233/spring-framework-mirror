74a53827771b5413c5e8abc382498e05ccd0a664
曾华(10039614) <zengh@glodon.com>
2022-09-01 12:26:22 +0800

    envTest

--- spring-reopen/src/main/java/com/github/zeng233/spring/container/context/annotation/config/env/EnvTest.java
+++ spring-reopen/src/main/java/com/github/zeng233/spring/container/context/annotation/config/env/EnvTest.java
@@ -3,6 +3,7 @@ package com.github.zeng233.spring.container.context.annotation.config.env;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.env.StandardEnvironment;
 
 /**
  * @author zenghua
@@ -20,6 +21,7 @@ public class EnvTest {
 	@Test
 	public void testSimple() {
 		EnvBean envBean = context.getBean("envBean", EnvBean.class);
+		StandardEnvironment environment = new StandardEnvironment();
 		System.out.println(envBean);
 	}
 }
